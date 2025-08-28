90c8bf23856d895639b5cf9c0a946684560b01ed
isninochka <ninaisaeva0919@gmail.com>
2025-08-28 20:25:25 +0200

    git init

---
+++ src/main/java/isaeva/Main.java
@@ -0,0 +1,7 @@
+package isaeva;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ task-service/src/test/java/isaeva/taskservice/TaskServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package isaeva.taskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TaskServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
