e33d84c7a71d0aa01748d287c0c5d61e2bf2bccd
isninochka <ninaisaeva0919@gmail.com>
2025-08-29 11:27:11 +0200

    added user service

---
+++ user-service/src/main/java/isaeva/userservice/service/UserService.java
@@ -0,0 +1,25 @@
+package isaeva.userservice.service;
+
+import isaeva.userservice.dto.UserRegistrationRequest;
+import isaeva.userservice.model.User;
+import isaeva.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(UserRegistrationRequest request) {
+        User user = new User();
+        user.setUsername(request.username());
+        user.setEmail(request.email());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        userRepository.save(user);
+    }
+
+}
