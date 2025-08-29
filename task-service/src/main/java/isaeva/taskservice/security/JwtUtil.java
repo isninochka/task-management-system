aa0837cbc7a392aa49c586f86389f476f8863ce1
isninochka <ninaisaeva0919@gmail.com>
2025-08-29 17:33:20 +0200

    added security

---
+++ task-service/src/main/java/isaeva/taskservice/security/JwtUtil.java
@@ -0,0 +1,48 @@
+package isaeva.taskservice.security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String jwtSecret = "mySuperSecretKeyForJwtWhichShouldBeVeryLong";
+
+    private final Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+    public String generateJwtToken(String username) {
+        long jwtExpiration = 3600000;
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date().getTime()+ jwtExpiration)))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key).build()
+                    .parseClaimsJwt(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
