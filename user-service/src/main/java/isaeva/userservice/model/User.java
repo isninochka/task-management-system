69c847bae115eda37bffe8458cf7e9a59ca001b1
isninochka <ninaisaeva0919@gmail.com>
2025-08-29 10:52:45 +0200

    add User model

---
+++ user-service/src/main/java/isaeva/userservice/model/User.java
@@ -0,0 +1,35 @@
+package isaeva.userservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique=true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
